c4820b9333ce966e8df660ec6968abe39e94622a
86151 <1767874487@qq.com>
2023-10-25 16:18:50 +0800

    third commit

---
+++ app/src/main/java/au/edu/federation/itech3107/studentattendance30395635/adapter/CourseAdapter.java
@@ -0,0 +1,25 @@
+package au.edu.federation.itech3107.studentattendance30395635.adapter;
+
+
+import androidx.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+import au.edu.federation.itech3107.studentattendance30395635.R;
+import au.edu.federation.itech3107.studentattendance30395635.room.CourseGroupBean;
+
+
+public class CourseAdapter extends BaseQuickAdapter<CourseGroupBean, BaseViewHolder> {
+
+    public CourseAdapter(int layoutResId, @Nullable List<CourseGroupBean> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, CourseGroupBean item) {
+        helper.setText(R.id.tv_item,item.getName());
+    }
+}
